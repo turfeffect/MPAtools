38b8bf7e9efbaff81d38ad9ecf4c5fae2e771b61
jcvdav <juanarlos.villader@gmail.com>
2016-03-13 16:37:47 -0700

    Changed scenario 3

--- R/mpa_xls2Rdata.R
+++ R/mpa_xls2Rdata.R
@@ -1,3 +1,4 @@
+mpa_xls2Rdata=function(a=1){
 library(readxl)
 
 area1=as.matrix(read_excel("data/areas.xlsx", sheet=1, col_names=FALSE))
@@ -8,11 +9,12 @@ area5=as.matrix(read_excel("data/areas.xlsx", sheet=5, col_names=FALSE))
 area6=as.matrix(read_excel("data/areas.xlsx", sheet=6, col_names=FALSE))
 
 save(area1, file="./data/area1.RData")
-save(area2, file="./data/area1.RData")
-save(area3, file="./data/area1.RData")
-save(area4, file="./data/area1.RData")
-save(area5, file="./data/area1.RData")
-save(area6, file="./data/area1.RData")
+save(area2, file="./data/area2.RData")
+save(area3, file="./data/area3.RData")
+save(area4, file="./data/area4.RData")
+save(area5, file="./data/area5.RData")
+save(area6, file="./data/area6.RData")
+}
 
 
 
