4a50975fc6ea27efc0414f6503bf606e01ba81aa
jcvdav <juanarlos.villader@gmail.com>
2016-03-17 17:24:10 -0700

    added closures to the model

--- R/mpa_sim.R
+++ R/mpa_sim.R
@@ -4,11 +4,13 @@
 #'
 #' @param area an N by M matrix with harvesting rates for fishing grounds (0 < harvesting rate <= 1) and MPAs (harvesting rate = 0) for the simulated area.
 #' @param nsteps Number of steps to run the simulation.
-#' @param r Intrinsic population growth rate.
+#' @param r Population growth rate.
 #' @param pop0 Initial population size. If single, assumes equal population size across all cells in area. If a matrix, it must have the same dimensions as area.
 #' @param K Carrying capacity
 #' @param mrate Movement rate expressed as a proportion (relative to 1). Indicates the proportion of organisms that will move out of a cell in one timestep.
-#' @param op A logical value that indicates if a filled.contour is to be created. WARNING: op = TRUE may make the simulation slow.
+#' @param op A logical value that indicates if a filled.contour is to be created. WARNING: op = TRUE may make the simulation slow. Alternatively, see closure.vec.
+#' @param closure.vec a vector that contains all nsteps in which a closure must occur. For example, closure.vec=c(1,2,3,10) will close the fishery in yers 1,2,3 consecutively, and then again on nstep 10.
+#' @param cf A single integer that indicates the frequency (how many n steps) in which a closure to fishing occurs (i.e. no fishing).
 #'
 #' @return results A list containing pop: a matrix of size N by M (same dimensions as area, above) with population size within each cell; and timeseries: a dataframe with 5 columns and nstep rows. The columns contain time (timeseries$time), population SIZE at each timestep (timeseries$pop), population DENSITY inside the MPA (timeseries$pop.in), population DENSITY outside the MPA (timeseries$pop.out) and yearly total catches (timeseries$total.catches).
 #'
@@ -18,7 +20,7 @@
 #' @export
 
 
-mpa_sim=function(area, nsteps, r, pop0, K, mrate, op=FALSE) {
+mpa_sim=function(area, nsteps, r, pop0, K, mrate, op=FALSE, cf=NULL, closure.vec=NULL) {
 
   #Making sure allinputs are correct
   check=mpa_check(area, nsteps, r, pop0, K, mrate)
@@ -31,75 +33,92 @@ mpa_sim=function(area, nsteps, r, pop0, K, mrate, op=FALSE) {
   nrows=size[1]
   ncols=size[2]
 
-   pop <- matrix(nrow=nrows, ncol=ncols)
+  pop <- matrix(nrow=nrows, ncol=ncols)
 
-   #set starting population in vector pop in each cell equal to K
-   pop[]=pop0
+  #set starting population in vector pop in each cell equal to K
+  pop[]=pop0
 
-   #vector left.cells storing the position of cells to the left
-   left.cells=c(ncols, 1:(ncols-1))
+  #vector left.cells storing the position of cells to the left
+  left.cells=c(ncols, 1:(ncols-1))
 
-   #vector right.cells storing the position of cells to the right
-   right.cells=c(2:ncols,1)
+  #vector right.cells storing the position of cells to the right
+  right.cells=c(2:ncols,1)
 
-   #vector up.cells storing the positon of cells up
-   up.cells=c(nrows,1:(nrows-1))
+  #vector up.cells storing the positon of cells up
+  up.cells=c(nrows,1:(nrows-1))
 
-   #vector down.cells storing the position of cells down
-   down.cells=c(2:nrows, 1)
+  #vector down.cells storing the position of cells down
+  down.cells=c(2:nrows, 1)
 
-   # These keep track of total population size in MPAs (pop.in) and total population size in fishing grounds (pop.out). ones is a matrix of size pop, that is used to normalize population by number of cells (i.e. population size/number of cells) later in the final plot of pop vs time.
-   inside=u.vec==0
-   outside=u.vec>0
-   ones=matrix(1,nrow=nrows, ncol=ncols)
-   pop.in=sum(pop[inside], na.rm=TRUE)/sum(ones[inside], na.rm=TRUE)
-   pop.out=sum(pop[outside], na.rm=TRUE)/sum(ones[outside], na.rm=TRUE)
-   pob=sum(pop, na.rm=TRUE)
-   total.catches=sum(u.vec*pop, na.rm=TRUE)
+  #set the vector where closures occur
+  if (!is.null(cf)){
+    closure=seq(cf,nsteps, by=cf)
+  }
 
-   if (op==TRUE){
-     windows()
-     filled.contour(pop, color=terrain.colors, asp=1, main="0", key.title=title(main="Density\n(Org/cell)"))
-   }
+  if (!is.null(closure.vec)){
+    closure=closure.vec
+  }
 
-   #loop through the time steps
-   for (i in 1:nsteps) {
+  if(is.null(closure.vec)){
+    closure=0
+  }
 
-      #Vector "leaving" of number leaving each cell is population size times diffusion rate
-     leaving=pop*mrate
+  # These keep track of total population size in MPAs (pop.in) and total population size in fishing grounds (pop.out). ones is a matrix of size pop, that is used to normalize population by number of cells (i.e. population size/number of cells) later in the final plot of pop vs time.
+  inside=u.vec==0
+  outside=u.vec>0
+  ones=matrix(1,nrow=nrows, ncol=ncols)
+  pop.in=sum(pop[inside], na.rm=TRUE)/sum(ones[inside], na.rm=TRUE)
+  pop.out=sum(pop[outside], na.rm=TRUE)/sum(ones[outside], na.rm=TRUE)
+  pob=sum(pop, na.rm=TRUE)
+  total.catches=sum(u.vec*pop, na.rm=TRUE)
 
-      #The number of immigrants is 1/4 those leaving cells to the
-      #left, 1/4 those leaving cells to the right, 1/4 those leaving up
-      #and 1/4 those leaving down. This is a complicated expression
-      #take a minute to think it through!
-     arriving=0.25*leaving[left.cells]+
-       0.25*leaving[right.cells]+
-       0.25*leaving[up.cells]+
-       0.25*leaving[down.cells]
+  if (op==TRUE){
+    windows()
+    filled.contour(pop, color=terrain.colors, asp=1, main="0", key.title=title(main="Density\n(Org/cell)"))
+  }
 
-      #surplus production from the logistic model
-      surplus=(r*pop)*(1-(pop/K))
+  #loop through the time steps
+  for (i in 1:nsteps) {
 
-      #catches = harvest rate in each cell times the population size
-      catches=u.vec*pop
+    #Vector "leaving" of number leaving each cell is population size times diffusion rate
+    leaving=pop*mrate
 
-      #update the population numbers
-      pop=pop+surplus-catches-leaving+arriving
+    #The number of immigrants is 1/4 those leaving cells to the
+    #left, 1/4 those leaving cells to the right, 1/4 those leaving up
+    #and 1/4 those leaving down. This is a complicated expression
+    #take a minute to think it through!
+    arriving=0.25*leaving[left.cells]+
+      0.25*leaving[right.cells]+
+      0.25*leaving[up.cells]+
+      0.25*leaving[down.cells]
 
-      #Also save two vectors containing population size inside MPA (pop.in) and population size outside MPA (pop.out), as well as total population size, and total catches.
-      pop.in[i+1]=sum(pop[inside], na.rm=TRUE)/sum(ones[inside], na.rm=TRUE)
-      pop.out[i+1]=sum(pop[outside], na.rm=TRUE)/sum(ones[outside], na.rm=TRUE)
-      pob[i+1]=sum(pop, na.rm=TRUE)
-      total.catches[i+1]=sum(catches, na.rm=TRUE)
+    #surplus production from the logistic model
+    surplus=(r*pop)*(1-(pop/K))
 
-      #continue plotting pop at time i if op=TRUE
-      if (op==TRUE){
+    #catches = harvest rate in each cell times the population size
+    catches=u.vec*pop
+
+    if (any(closure==i)){
+      catches=0
+    }
+
+    #update the population numbers
+    pop=pop+surplus-catches-leaving+arriving
+
+    #Also save two vectors containing population size inside MPA (pop.in) and population size outside MPA (pop.out), as well as total population size, and total catches.
+    pop.in[i+1]=sum(pop[inside], na.rm=TRUE)/sum(ones[inside], na.rm=TRUE)
+    pop.out[i+1]=sum(pop[outside], na.rm=TRUE)/sum(ones[outside], na.rm=TRUE)
+    pob[i+1]=sum(pop, na.rm=TRUE)
+    total.catches[i+1]=sum(catches, na.rm=TRUE)
+
+    #continue plotting pop at time i if op=TRUE
+    if (op==TRUE){
       filled.contour(pop, col=rainbow(100), asp=1, main=i, key.title=title(main="Density\n(Org/cell)"))
-      }
-   }
+    }
+  }
 
-   #Create a list of results
-   results=list(pop=pop, time.series=data.frame(time=seq(0:nsteps), pop=pob, pop.in, pop.out, total.catches))
+  #Create a list of results
+  results=list(pop=pop, time.series=data.frame(time=seq(0:nsteps), pop=pob, pop.in, pop.out, total.catches))
 
-   return(results)
+  return(results)
 }
