fdb4bc98a6a5763a5b9bb13d3b2235b340eb985f
jcvdav <juanarlos.villader@gmail.com>
2016-05-23 19:37:16 -0700

    Added function to calculate mean trophic level. Modified base for a, b, and TL

---
+++ R/trophic.R
@@ -0,0 +1,27 @@
+#' Calculate mean trophic level
+#'
+#' @description Calulates mean trophic level, by year and transect, for each location.
+#'
+#' @param data A dataframe that contains at least columns of Year, Zone (e.g. reserve or control site), Transect Number, GenusSpecies.
+#' @param site A quoted string that indicates the site. Options are "Rosario", "IslaNatividad", "IslaMagdalena".
+#'
+#' @return size A dataframe containing year, zone (insido or outside the reserve), transect number, species, and the statistic required.
+
+trophic=function(data, site){
+  library(dplyr)
+  library(tidyr)
+  library(reshape)
+
+  data(abtl)
+
+  data=untable(df=data, n=data$Abundancia) %>%
+    left_join(abtl, by="GeneroEspecie")
+  B=data %>%
+    filter(Site==site) %>%
+    group_by(Year, Zone, TransectNumber) %>%
+    summarize(mean=mean(TL, na.rm=TRUE))
+
+
+  return(B)
+
+}
