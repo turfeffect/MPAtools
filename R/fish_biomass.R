570fe76eea2bdad171887193536bdb1559e0542e
jcvdav <juanarlos.villader@gmail.com>
2016-05-23 19:13:53 -0700

    Added database with allometric parameters and TL. Also finished biomass function.

--- R/fish_biomass.R
+++ R/fish_biomass.R
@@ -7,3 +7,31 @@
 #' @param species A quoted string that indicates a species for which density should be calulated. Common cases include "Paralabrax clathratus", "Semicossipjus pulcher", or "Anisotremus davidsonii".
 #'
 #' @return size A dataframe containing year, zone (insido or outside the reserve), transect number, species, and the statistic required.
+
+fish_biomass=function(data, site, species=NULL){
+  library(dplyr)
+  library(tidyr)
+  library(reshape)
+
+  data(abtl)
+
+  data=untable(df=data, n=data$Abundancia) %>%
+    left_join(abtl, by="GeneroEspecie") %>%
+    mutate(W=a*(SizeClass^b))
+
+  if(is.null(species)){
+    B=data %>%
+      filter(Site==site) %>%
+      group_by(Year, Zone, TransectNumber, GeneroEspecie) %>%
+      summarize(sum(W))
+  } else {
+    B=data %>%
+      filter(Site==site) %>%
+      filter(GeneroEspecie==species) %>%
+      group_by(Year, Zone, TransectNumber, GeneroEspecie) %>%
+      summarize(sum(W))
+  }
+
+  return(B)
+
+}
