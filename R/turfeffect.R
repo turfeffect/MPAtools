72c7f2f8d5939d9a1fc79664f75246c8a3e3df98
jcvdav <juancarlos.villader@gmail.com>
2016-12-07 19:24:06 -0800

    turfeffect fxn incorporates Temp, Vis, and Depth into the mlr analysis

--- R/turfeffect.R
+++ R/turfeffect.R
@@ -22,12 +22,15 @@ turfeffect <- function (data, reserve, control){
   dummy.data <- data.frame(Ano = data$Ano,
                            Zona = NA,
                            Sitio = data$Sitio,
-                           Indicador = data$Indicador)
+                           Indicador = data$Indicador,
+                           Temperatura = data$Temperatura,
+                           Profundidad = data$Profundidad,
+                           Visibilidad = data$Visibilidad)
 
   dummy.data$Zona[data$Zonificacion == "Reserva"] <- 0
   dummy.data$Zona[data$Zonificacion == "Control"] <- 1
 
-  model <- lm(Indicador ~ Ano * Zona, data = dummy.data)
+  model <- lm(Indicador ~ Ano * Zona + Temperatura + Visibilidad + Profundidad, data = dummy.data)
 
   return(model)
 }
