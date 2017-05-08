a593511df6d9566ff69b0f787f07f1f370040f2f
jcvdav <juancarlos.villader@gmail.com>
2017-05-08 11:06:11 -0700

    Added a non-exported function that defines which covariates to drop if multiple NA cases exist, closes #9

---
+++ R/define_covars.R
@@ -0,0 +1,20 @@
+define_covars <- function(data, cutoff = 0.1){
+  data_length <- dim(data)[1]
+
+  naT <- sum(is.na(data$Temperatura))/data_length
+  naP <- sum(is.na(data$Visibilidad))/data_length
+  naV <- sum(is.na(data$Profundidad))/data_length
+
+  naCovars <- data.frame(Temperatura = naT, Visibilidad = naV, Profundidad = naP) %>%
+    gather(covar, value) %>%
+    filter(value <= cutoff)
+
+  if(dim(naCovars)[1] >= 1){
+    return(naCovars$covar)
+  }
+
+  if(dim(naCovars)[1] == 0){
+    return("None")
+  }
+
+}
