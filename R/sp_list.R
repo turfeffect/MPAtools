4f937c2a7f049350816205ebd2bda12402abbb90
jcvdav <juancarlos.villader@gmail.com>
2017-06-01 12:37:43 -0700

    Updated documentation

--- R/sp_list.R
+++ R/sp_list.R
@@ -1,37 +1,37 @@
 #' Title
 #'
-#' @param fish 
-#' @param invert 
-#' @param rc 
+#' @param fish
+#' @param invert
+#' @param rc
 #'
-#' @return
+#' @return sp_list
 #' @export
 #'
 #' @examples
-#' 
+#'
 sp_list <- function(fish, invert, rc) {
-  
+
   sp_list <- fish %>%
-    filter(!is.na(Abundancia), RC %in% rc)  %>% 
-    {.$GeneroEspecie}%>% 
-    unique() %>% 
-    sort() %>% 
-    data.frame() %>% 
-    mutate(class = "fish") %>% 
+    filter(!is.na(Abundancia), RC %in% rc)  %>%
+    {.$GeneroEspecie}%>%
+    unique() %>%
+    sort() %>%
+    data.frame() %>%
+    mutate(class = "fish") %>%
     magrittr::set_colnames(value = c("sp", "class"))
-  
+
   if (!is.null(invert)) {
-    invert <- invert %>% 
-      filter(!is.na(Abundancia), RC %in% rc) %>% 
-      {.$GeneroEspecie}%>% 
-      unique() %>% 
-      sort() %>% 
-      data.frame() %>% 
-      mutate(class = "invert") %>% 
+    invert <- invert %>%
+      filter(!is.na(Abundancia), RC %in% rc) %>%
+      {.$GeneroEspecie}%>%
+      unique() %>%
+      sort() %>%
+      data.frame() %>%
+      mutate(class = "invert") %>%
       magrittr::set_colnames(value = c("sp", "class"))
-    
+
     sp_list <- rbind(sp_list, invert)
   }
-  
+
   return(sp_list)
 }
