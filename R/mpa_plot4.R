aecca51d2b06d40fd580a3b3421513651b61e00f
jcvdav <juancarlos.villader@gmail.com>
2017-06-03 19:53:10 -0700

    Changed order of geoms to have better visualization of data

--- R/mpa_plot4.R
+++ R/mpa_plot4.R
@@ -16,10 +16,10 @@ mpa_plot4 <- function(model, y.lab = NULL) {
   p <- model$model %>%
     mutate(Ano = as.numeric(as.character(Ano))) %>%
     ggplot(aes(x = Ano, y = Indicador, color = Zona, fill = Zona)) +
-    geom_jitter(height = 0, width = 0.1, size = 1) +
+    stat_summary(fun.data = mean_se, geom = "ribbon", alpha = 0.5) +
     stat_summary(geom = "point", fun.y = mean, color = "black", size = 2, alpha = 0.5) +
     stat_summary(geom = "line", fun.y = mean, color = "black", alpha = 0.5) +
-    stat_summary(fun.data = mean_se, geom = "ribbon", alpha = 0.5) +
+    geom_jitter(height = 0, width = 0.1, size = 1) +
     scale_color_brewer(palette = "Set1", direction = -1) +
     scale_fill_brewer(palette = "Set1", direction = -1) +
     theme_bw() + labs(x = "Ano", y = y.lab)
