13050cd611b61d1b80013329ab16ab7cd15e4e68
jcvdav <juancarlos.villader@gmail.com>
2017-06-11 14:43:05 -0700

    Fixed incorrect identification of covariates

--- R/define_covars.R
+++ R/define_covars.R
@@ -1,9 +1,19 @@
+#' Title
+#'
+#' @param data
+#' @param cutoff
+#'
+#' @return
+#' @export
+#'
+#' @examples
 define_covars <- function(data, cutoff = 0.1){
+
   data_length <- dim(data)[1]
 
   naT <- sum(is.na(data$Temperatura))/data_length
-  naP <- sum(is.na(data$Visibilidad))/data_length
-  naV <- sum(is.na(data$Profundidad))/data_length
+  naV <- sum(is.na(data$Visibilidad))/data_length
+  naP <- sum(is.na(data$Profundidad))/data_length
 
   naCovars <- data.frame(Temperatura = naT, Visibilidad = naV, Profundidad = naP) %>%
     gather(covar, value) %>%
